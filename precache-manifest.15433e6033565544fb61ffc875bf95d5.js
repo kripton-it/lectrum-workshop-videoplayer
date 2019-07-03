74b430c55eb5d8c1338691adbd0fcb0dc74170a4
Mikhail Shardin <kriptonit1988@gmail.com>
2019-07-03 13:29:37 +0500

    Updates

---
+++ precache-manifest.15433e6033565544fb61ffc875bf95d5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4ebda65629f498d884889392f289816",
+    "url": "/videoplayer/static/media/spring.e4ebda65.mp4"
+  },
+  {
+    "revision": "14019bf1073f26b4c937",
+    "url": "/videoplayer/static/js/runtime~main.14019bf1.js"
+  },
+  {
+    "revision": "3e2f59e5fc05d9dae659",
+    "url": "/videoplayer/static/js/main.3e2f59e5.chunk.js"
+  },
+  {
+    "revision": "0e3da0ea298c017f95c4",
+    "url": "/videoplayer/static/js/2.0e3da0ea.chunk.js"
+  },
+  {
+    "revision": "3e2f59e5fc05d9dae659",
+    "url": "/videoplayer/static/css/main.46ff59fc.chunk.css"
+  },
+  {
+    "revision": "0e3da0ea298c017f95c4",
+    "url": "/videoplayer/static/css/2.6d08c180.chunk.css"
+  },
+  {
+    "revision": "42d3dfa481efdb414cb600e6570258c6",
+    "url": "/videoplayer/index.html"
+  }
+];
